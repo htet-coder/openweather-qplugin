9a1c724f7a3e0e7c02ef781049f63bd986e93d58
htet-coder <71670671+htet-coder@users.noreply.github.com>
2023-10-16 09:11:19 +0000

    Added source plugin code for version control

---
+++ urls.py
@@ -0,0 +1,8 @@
+"""Definitions for urls."""
+__author__ = 'Htet Arkar Soe'
+__date__ = '2022-02-12'
+__copyright__ = '(C) 2022 by Htet Arkar Soe'
+
+
+DOC_PLUGIN_URL = ('https://github.com/geohtet/OpenWeatherDataDownloaderAlgorithm/blob/main/README.md')
+
